8a28ac1b563e3e4d3dd7453ab18739cae8af35b4
Kyun Heo <kyun2dot@gmail.com>
2022-07-16 22:59:16 +0900

    feat: initialize football page

--- apps/football/pages/_app.tsx
+++ apps/football/pages/_app.tsx
@@ -1,7 +1,5 @@
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [a, setA] = useState(0);
-
   return <Component {...pageProps} />;
 }

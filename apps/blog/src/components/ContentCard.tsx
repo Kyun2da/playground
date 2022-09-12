5aa4507ce604e085d8b981f05ecb9ab151a44e9f
Kyun Heo <kyun2dot@gmail.com>
2022-09-12 17:04:24 +0900

    feat: add Link Component for prefetch

--- apps/blog/src/components/ContentCard.tsx
+++ apps/blog/src/components/ContentCard.tsx
@@ -1,7 +1,7 @@
 import { Post } from '@interfaces/Post';
 import { Card, Col, Row, Text } from '@nextui-org/react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { ComponentProps } from 'react';
 
 import { CategoryBadge } from './CategoryBadge';
@@ -11,50 +11,43 @@ interface Props extends ComponentProps<typeof Card> {
 }
 
 export function ContentCard({ post, ...props }: Props) {
-  const router = useRouter();
-
   return (
-    <Card
-      hoverable
-      clickable
-      onClick={() => {
-        router.push(`/posts/${post.slug}`);
-      }}
-      {...props}
-    >
-      <Row>
-        <div
-          style={{
-            borderRadius: '12px',
-            overflow: 'hidden',
-            backgroundColor: '#f1f3f5',
-            marginRight: 12,
-          }}
-        >
-          <Image
-            src={post.coverImage}
-            objectFit="scale-down"
-            width={200}
-            height={200}
-            alt={post.title}
-          />
-        </div>
-        <Col>
-          <Text h2>{post.title}</Text>
-          {post.categories.map(category => {
-            return (
-              <CategoryBadge key={category} size="xs">
-                {category}
-              </CategoryBadge>
-            );
-          })}
-          <Text>{post.excerpt}</Text>
-          <Row gap={1} css={{ justifyContent: 'flex-end' }}>
-            <Text>{post.date}</Text>
-            <Text>읽는데 {post.time}분</Text>
-          </Row>
-        </Col>
-      </Row>
-    </Card>
+    <Link href={`/posts/${post.slug}`}>
+      <Card hoverable clickable {...props}>
+        <Row>
+          <div
+            style={{
+              borderRadius: '12px',
+              overflow: 'hidden',
+              backgroundColor: '#f1f3f5',
+              marginRight: 12,
+            }}
+          >
+            <Image
+              src={post.coverImage}
+              objectFit="scale-down"
+              width={200}
+              height={200}
+              alt={post.title}
+            />
+          </div>
+          <Col>
+            <Text h2>{post.title}</Text>
+            {post.categories.map(category => {
+              return (
+                <CategoryBadge key={category} size="xs">
+                  {category}
+                </CategoryBadge>
+              );
+            })}
+            <Text>{post.excerpt}</Text>
+            <Row gap={1} css={{ justifyContent: 'flex-end' }}>
+              <Text>{post.date}</Text>
+              <Text>읽는데 {post.time}분</Text>
+            </Row>
+          </Col>
+        </Row>
+      </Card>
+    </Link>
   );
 }

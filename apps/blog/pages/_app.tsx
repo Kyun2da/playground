63b4af399651771be5b71a318750ea3c14d03eb4
Kyun Heo <kyun2dot@gmail.com>
2023-01-02 23:41:28 +0900

    fix : initial css loading

--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { darkTheme, lightTheme } from '@components/theme/theme';
-import { CssBaseline, NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 
@@ -14,7 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
       }}
     >
       <NextUIProvider>
-        {CssBaseline.flush()}
         <Component {...pageProps} />
       </NextUIProvider>
     </ThemeProvider>

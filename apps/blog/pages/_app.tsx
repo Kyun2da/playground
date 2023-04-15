c03dec29b46d03c05158a7b254fbaf5351b9a062
Kyun Heo <kyun2dot@gmail.com>
2023-04-16 04:17:17 +0900

    fix: ssr issue

--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { darkTheme, lightTheme } from '@components/theme/theme';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, useSSR } from '@nextui-org/react';
 import * as gtag from '@utils/gtag';
 import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'next-themes';
@@ -7,25 +7,28 @@ import { AppProps } from 'next/app';
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { isBrowser } = useSSR();
+
   return (
-    <ThemeProvider
-      defaultTheme="system"
-      attribute="class"
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+    isBrowser && (
+      <ThemeProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -33,12 +36,13 @@ export default function App({ Component, pageProps }: AppProps) {
               page_path: window.location.pathname,
             });
           `,
-        }}
-      />
-      <NextUIProvider>
-        <Component {...pageProps} />
-        <Analytics />
-      </NextUIProvider>
-    </ThemeProvider>
+          }}
+        />
+        <NextUIProvider>
+          <Component {...pageProps} />
+          <Analytics />
+        </NextUIProvider>
+      </ThemeProvider>
+    )
   );
 }

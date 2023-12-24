751111f9595a177c12f2816bf49d55609b41d000
Kyun2da <kyun2dot@gmail.com>
2023-12-24 12:22:57 +0900

    feat: og image test

--- apps/blog/app/robots.ts
+++ apps/blog/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/api/og/*'],
     },
     sitemap: 'https://kyun2da.dev/sitemap.xml',
   };

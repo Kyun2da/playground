1e24754747eb0cc0f20955df2e632f28d47c2180
Kyun2da <kyun2dot@gmail.com>
2024-03-09 18:52:27 +0900

    fix: auto Refresh잘 되도록 폴더경로 수정

--- apps/blog/watcher.js
+++ apps/blog/watcher.js
@@ -9,7 +9,7 @@ const chokidar = require('chokidar');
 const wss = new WebSocketServer({ port: 3001 });
 const watchCallbacks = [];
 
-chokidar.watch('./public').on('all', event => {
+chokidar.watch('./_posts').on('all', event => {
   if (event === 'change') {
     watchCallbacks.forEach(cb => cb());
   }

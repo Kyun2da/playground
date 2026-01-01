4cd1963c3892708bef0c6b07e7283130a92dab4c
Kyun2da <kyun2dot@gmail.com>
2026-01-02 01:41:09 +0900

    feat: RSS에 썸네일 이미지 추가

--- apps/blog/src/pages/rss.xml.js
+++ apps/blog/src/pages/rss.xml.js
@@ -7,15 +7,29 @@ export async function GET(context) {
     new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
   );
 
+  const siteUrl = context.site || 'https://kyun2da.dev';
+
   return rss({
     title: 'Kyun2da Blog',
     description: '허균의 블로그',
-    site: context.site,
+    site: siteUrl,
     items: sortedPosts.map((post) => ({
       title: post.data.title,
       pubDate: new Date(post.data.date),
       description: post.data.excerpt,
       link: `/posts/${post.slug}/`,
+      enclosure: post.data.coverImage ? {
+        url: `${siteUrl}${post.data.coverImage}`,
+        length: 0,
+        type: 'image/jpeg',
+      } : undefined,
+      customData: post.data.coverImage
+        ? `<media:content url="${siteUrl}${post.data.coverImage}" medium="image" />`
+        : '',
     })),
+    customData: `<language>ko-KR</language>`,
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
   });
 }
